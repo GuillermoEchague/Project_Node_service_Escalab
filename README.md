8d9cbd6ed94af39aaacded2e0179bcf807037989
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2021-10-22 14:26:17 -0300

    Mostrando el Avatar del usuario 21

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
+# Desarrollo Rest Server Proyecto curso Node ESCALAB
 
 
-## Dependencias
-
+## Instalar Dependencias
+### Dependencias
+```bash
 yarn add bcrypt-nodejs
 yarn add body-parser
 yarn add connect-multiparty
@@ -10,3 +12,73 @@ yarn add jwt-simple
 yarn add moment
 yarn add mongoose
 yarn add nodemon
+```
+
+## Clonar Repositorio
+
+```bash
+git clone ...
+```
+
+## Recontruir modulos de Node
+
+```bash
+yarn install
+```
+
+## Generar Tags del proyecto
+
+```bash
+git tag -a v1.0.0 -m "Inicio WebServer"
+git push --tags
+
+# delete local tag 'v1.0.0'
+git tag -d v1.0.0
+# delete remote tag 'v1.0.0' (eg, GitHub version too)
+git push origin :refs/tags/v1.0.0
+
+# alternative approach
+git push --delete origin tagName
+git tag -d tagName
+
+git rm .env --cached (Borrar seguimiento)
+```
+
+## Subir a Heroku
+
+```bash
+heroku git:clone -a imageniacellphone
+
+# Repetir proceso luego de cambio en el proyecto
+
+git branch
+git push heroku main
+```
+
+## Crear - borrar variables de entorno Heroku
+
+```bash
+heroku --version
+heroku config
+heroku config:set nombre="Guillermo"
+heroku config:unset nombre
+```
+
+## Logs en heroku
+
+```bash
+heroku logs -n 100 -a
+heroku logs -n 100 --tail
+```
+
+## Producción 
+
+```bash
+https://imageniacellphone.herokuapp.com/....
+```
+
+## Documentación Postman
+
+```bash
+https://documenter.getpostman.com/view/7460598/UV5ZAvwU
+```
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const bcrypt = require("bcrypt-nodejs");
 const jwt = require("../services/jwt");
 const User = require("../models/user");
@@ -44,7 +46,6 @@ function signUp(req, res) {
     }
   }
 
-
 function signIn(req, res) {
     const params = req.body;
     const email = params.email;
@@ -72,14 +73,135 @@ function signIn(req, res) {
                 })
               }
             }
-          })
+          });
         }
       }
     });
   }
 
+function getUsers(req, res) {
+  User.find().then(users => {
+    if(!users) {
+      res.status(404).send({message: "No se encontro ningun usuario."});
+    } else {
+      res.status(200).send({users});
+    }
+  })
+}
+
+function getUsersActive(req, res) {
+  const query = req.query;
+
+  User.find({active: query.active}).then(users => {
+    if(!users) {
+      res.status(404).send({message: "No se encontro ningun usuario."});
+    } else {
+      res.status(200).send({users});
+    }
+  })
+}
+
+function uploadAvatar(req, res) {
+  const params = req.params;
+
+  User.findById({ _id: params.id }, (err, userData) => {
+    if (err) {
+      res.status(500).send({ message: "Error del servidor." });
+    } else {
+      if (!userData) {
+        res.status(404).send({ message: "Nose ha encontrado ningun usuario." });
+      } else {
+        let user = userData;
+
+        if (req.files) {
+          let filePath = req.files.avatar.path;
+          let fileSplit = filePath.split("\\");
+          let fileName = fileSplit[2];
+
+          let extSplit = fileName.split(".");
+          let fileExt = extSplit[1];
+
+          if (fileExt !== "png" && fileExt !== "jpg") {
+            res.status(400).send({
+              message:
+                "La extension de la imagen no es valida. (Extensiones permitidas: .png y .jpg)"
+            });
+          } else {
+            user.avatar = fileName;
+            User.findByIdAndUpdate(
+              { _id: params.id },
+              user,
+              (err, userResult) => {
+                if (err) {
+                  res.status(500).send({ message: "Error del servidor." });
+                } else {
+                  if (!userResult) {
+                    res
+                      .status(404)
+                      .send({ message: "No se ha encontrado ningun usuario." });
+                  } else {
+                    res.status(200).send({ avatarName: fileName });
+                  }
+                }
+              }
+            );
+          }
+        }
+      }
+    }
+  });
+}
+
+function getAvatar(req, res) {
+  const avatarName = req.params.avatarName;
+  const filePath = "./uploads/avatar/" + avatarName;
+
+  fs.exists(filePath, exists => {
+    if (!exists) {
+      res.status(404).send({ message: "El avatar que buscas no existe." });
+    } else {
+      res.sendFile(path.resolve(filePath));
+    }
+  });
+}
+
+async function updateUser(req, res) {
+  let userData = req.body;
+  userData.email = req.body.email;
+  const params = req.params;
+
+  if (userData.password) {
+    await bcrypt.hash(userData.password, null, null, (err, hash) => {
+      if (err) {
+        res.status(500).send({ message: "Error al encriptar la contraseña." });
+      } else {
+        userData.password = hash;
+      }
+    });
+  }
+
+  User.findByIdAndUpdate({ _id: params.id }, userData, (err, userUpdate) => {
+    if (err) {
+      res.status(500).send({ message: "Error del servidor." });
+    } else {
+      if (!userUpdate) {
+        res
+          .status(404)
+          .send({ message: "No se ha encontrado ningun usuario." });
+      } else {
+        res.status(200).send({ message: "Usuario actualizado correctamente." });
+      }
+    }
+  });
+}
+
 
 module.exports = {
     signUp,
-    signIn
+    signIn,
+    getUsers,
+    getUsersActive,
+    uploadAvatar,
+    getAvatar,
+    updateUser
 };
