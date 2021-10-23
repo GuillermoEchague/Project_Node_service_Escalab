16a721b2998421a502d4c56172b77474cfad4957
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2021-10-23 03:19:49 -0300

    Menu ok

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const {API_VERSION} = require('./config');
 // Load routing
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const menuRoutes = require('./routes/menu');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -32,6 +33,6 @@ app.use(cors());
 // Router basic
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, menuRoutes);
 
 module.exports = app;
-
